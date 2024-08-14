5671d43d71f400d8c8d17141a3bbee484e262d49
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-14 11:13:19 +0000

    Basics of datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all javascript code as newer version
+
+// alert(6 + 6) //this wiil throw error because we are using node.js, and not browser 
+
+console.log(3 
+    + 
+    3) // we should not write code like this because code readability should be high
+
+ console.log("Sunil");
+    
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
