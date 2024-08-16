e8a332e7a2454bc89e85dd5bd6c4cc9cdc4feece
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-16 05:53:37 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+// let score = 33
+// console.log(typeof score); //number
+// console.log(typeof(score)); //number
+
+// let score = "33"
+// console.log(typeof score); // string
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+
+// let score = "33abc"
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+// let score = null
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 0
+
+
+// let score = undefined
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+
+// let score = true
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 1
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // false
+
+// 1 => true; 0 => false
+// "" => false
+// "Sunil" => true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber); //33
+// console.log(stringNumber); //33
+
+
+
+
+
+
+
