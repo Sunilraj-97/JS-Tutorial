fc06382f352e9f4cfaad14bbe6e4c5de602ac2a8
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-19 10:43:30 +0000

    Coomit: Basics of dataTypes and summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,52 @@
+// Primitive
+
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100
+// console.log(typeof score); //number
+
+const scoreValue = 100.3
+console.log(typeof scoreValue); //number
+
+
+const isLoggedIn = false
+console.log(typeof isLoggedIn); //boolean
+const outsideTemp = null
+console.log(typeof outsideTemp); //object
+
+let userEmail; // undefined
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId); // false
+
+const bigNumber = 34543534534534343n
+
+console.log(typeof bigNumber); //bigint
+
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Raju", "sunilraj", "hulk"]
+// console.log(typeof heros); // object
+
+const values = {
+    name: "Sunil",
+    age: 22
+}
+
+// console.log(typeof values); // object
+
+const myFunction = function(){
+    console.log("Hello Sunil");    
+}
+
+console.log(typeof myFunction)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
