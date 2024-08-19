213aec71ff81e4a80a6cab72884cd25addbb7f61
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-19 06:48:58 +0000

    Commit: Basics of comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1); // true
+// console.log("02">1); // true
+
+
+// --------------------------------------------------------------------
+
+// console.log(null > 0) //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+
+//The reason id that an equality check == and comaprision 
+// > <> = <= works differently 
+// Comparision convert null to a number, treating it as 0 That's why (3) 
+// null >= 0 is true and (1) null > 0 is false
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
