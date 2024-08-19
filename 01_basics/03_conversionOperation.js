53ffea1ce79983a75d8d40475aabaabb0cd15d57
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-19 05:57:03 +0000

    Commit: Basics of operations commited

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -52,6 +52,60 @@
 // console.log(stringNumber); //33
 
 
+// ******************* Operations *******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// ---------------------------------------------
+
+let str1 = "hello"
+let str2 = " Sunil"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// ----------------------------------------------
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// ------------------------------------------------
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
+
+
+
+
+
+
 
 
 
