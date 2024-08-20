2c66a831ef55a086a7a2b6bd22dff92ad207527f
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-20 05:51:46 +0000

    Commit: Types of memory learnt

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -1,4 +1,4 @@
-// Primitive
+//****************** */ Primitive *************************
 
 // 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
 
@@ -6,29 +6,29 @@ const score = 100
 // console.log(typeof score); //number
 
 const scoreValue = 100.3
-console.log(typeof scoreValue); //number
+// console.log(typeof scoreValue); //number
 
 
 const isLoggedIn = false
-console.log(typeof isLoggedIn); //boolean
+// console.log(typeof isLoggedIn); //boolean
 const outsideTemp = null
-console.log(typeof outsideTemp); //object
+// console.log(typeof outsideTemp); //object
 
 let userEmail; // undefined
 
 const id = Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id === anotherId); // false
+// console.log(id === anotherId); // false
 
 const bigNumber = 34543534534534343n
 
-console.log(typeof bigNumber); //bigint
+// console.log(typeof bigNumber); //bigint
 
 
 
 
-// Reference (Non primitive)
+//************** */ Reference (Non primitive) *******************
 
 // Array, Objects, Functions
 
@@ -42,11 +42,41 @@ const values = {
 
 // console.log(typeof values); // object
 
-const myFunction = function(){
-    console.log("Hello Sunil");    
+const myFunction = function () {
+    console.log("Hello Sunil");
 }
 
-console.log(typeof myFunction) // function
+// console.log(typeof myFunction) // function
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// *************************** Heap ** Stack *********************************************
+
+// There are two types of memory
+// 1.Satck (Primitive)  
+
+let myName = "Sunilraj"
+
+let myAnotherName = myName
+myAnotherName = "Hemanth"
+
+console.log(myName);
+console.log(myAnotherName);
+
+// 2.Heap (Reference or Non Primitive)
+
+let userOne = {
+    name: "Sunil",
+    age: 23
+}
+
+let userTwo = userOne
+
+userTwo.name = "Hemanth"
+
+console.log(userOne);
+console.log(userTwo);
+
+
 
