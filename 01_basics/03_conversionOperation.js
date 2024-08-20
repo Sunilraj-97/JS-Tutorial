0e70cf031692725e517e2408b9f1bea8dc03b936
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-20 05:50:38 +0000

    Commit: Study link updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -96,7 +96,7 @@ gameCounter++;
 console.log(gameCounter);
 
 // link to study
-// https://tc39.es/ecma262/#sec-abstract-operations
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
 
 
