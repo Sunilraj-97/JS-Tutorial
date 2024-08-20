51092423f8c358ee1c4d08cfd1f64186b39bc2bf
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-20 05:51:08 +0000

    Commi: Console result added

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -19,9 +19,9 @@
 // Comparision convert null to a number, treating it as 0 That's why (3) 
 // null >= 0 is true and (1) null > 0 is false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
 // === 
 
