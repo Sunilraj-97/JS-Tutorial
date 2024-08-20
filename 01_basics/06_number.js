bef0292ff0716f18deedc14d58cfc5db7eebed78
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-20 09:56:37 +0000

    Basics of number and math

---
+++ 01_basics/06_number.js
@@ -0,0 +1,24 @@
+let score = 23
+// console.log(score);
+
+let newNumber = new Number(23)
+
+// console.log(newNumber.toString());
+// console.log(newNumber.toFixed(2));
+
+let otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(2)); // 24
+// console.log(otherNumber.toPrecision(4)); // 23.90
+// console.log(otherNumber.toPrecision(3)); // 23.9
+
+let paisa = 10000000
+// console.log(paisa.toLocaleString('en-IN'));
+
+// ************************Maths*****************************
+
+//  later i need to watch study link
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+
