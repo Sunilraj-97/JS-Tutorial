c3c0c4d77a57bb8d7dec234efcfddeb9c3423f14
Sunilraj-97 <103097681+Sunilraj-97@users.noreply.github.com>
2024-08-20 07:39:23 +0000

    Commit: Basics of String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+let name = "Sunil "
+let repoCount = 50
+
+// console.log(name + repoCount + " Value");  //❌
+
+// console.log(`my name is ${name} and my count is ${repoCount}`); //👍
+
+let gameName = new String("Sunilraj")
+
+// console.log(gameName[0]) // S
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.indexOf('r'));
+
+let index = 3
+// console.log(`The character at index ${index} is ${gameName.charAt(index)}`); //charAt()
+// console.log(`Character code ${gameName.charCodeAt(index)} is equal to ${gameName.charAt(index,)}`,);  //charCodeAt()
+
+
+// Study link String methods
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/at
+
+
+
+
+
